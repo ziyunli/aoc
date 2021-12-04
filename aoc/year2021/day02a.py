0000000a413f99f08ba8fb276135a6ab34244491
Ziyun Li <me@ziyunli.dev>
2021-12-03 22:07:58 -0500

    day02a

---
+++ aoc/year2021/day02a.py
@@ -0,0 +1,57 @@
+from collections import namedtuple
+from enum import Enum, auto
+from typing import List
+
+
+class AutoName(Enum):
+    def _generate_next_value_(name, start, count, last_values):
+        return name
+
+
+class Direction(AutoName):
+    FORWARD = auto()
+    BACKWARD = auto()
+    UP = auto()
+    DOWN = auto()
+
+
+Command = namedtuple("Command", ['direction', 'step'])
+Position = namedtuple("Position", ['x', 'y'])
+
+
+def parse_commands(file):
+    ret = []
+    for line in file:
+        x, y = line.strip().split()
+
+        cmd = Command(direction=Direction(x.upper()), step=int(y))
+        ret.append(cmd)
+    return ret
+
+
+def move(pos: Position, cmd: Command) -> Position:
+    match cmd.direction:
+        case Direction.FORWARD:
+            return Position(x=pos.x + cmd.step, y=pos.y)
+        case Direction.BACKWARD:
+            return Position(x=pos.x - cmd.step, y=pos.y)
+        case Direction.UP:
+            return Position(x=pos.x, y=pos.y - cmd.step)
+        case Direction.DOWN:
+            return Position(x=pos.x, y=pos.y + cmd.step)
+
+
+def move_submarine(start: Position, commands: List[Command]) -> Position:
+    curr = start
+    for cmd in commands:
+        curr = move(curr, cmd)
+    return curr
+
+
+if __name__ == "__main__":
+    with open("input/2021/day02.txt") as reader:
+        commands = parse_commands(reader)
+        print(commands)
+        final = move_submarine(Position(x=0, y=0), commands)
+
+        print(final.x * final.y)
