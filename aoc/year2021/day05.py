0000000179f7f8984c48734b15aa7306af738daf
Ziyun Li <me@ziyunli.dev>
2021-12-05 22:44:24 -0500

    day05b

--- aoc/year2021/day05.py
+++ aoc/year2021/day05.py
@@ -20,15 +20,25 @@ class Segment:
     def is_vertical(self):
         return self.start.x == self.end.x
 
-    def points(self):
+    def is_diagonal(self):
+        return abs(self.end.x - self.start.x) == abs(self.end.y - self.start.y)
+
+    def points(self) -> List[Point]:
         if self.is_horizontal():
             step = 1 if self.end.x > self.start.x else -1
             return [Point(x, self.start.y) for x in range(self.start.x, self.end.x + step, step)]
         elif self.is_vertical():
             step = 1 if self.end.y > self.start.y else -1
             return [Point(self.start.x, y) for y in range(self.start.y, self.end.y + step, step)]
+        elif self.is_diagonal():
+            step_x = 1 if self.end.x > self.start.x else -1
+            step_y = 1 if self.end.y > self.start.y else -1
+            ret = []
+            for n in range(abs(self.end.x - self.start.x) + 1):
+                ret.append(Point(self.start.x + step_x * n, self.start.y + step_y * n))
+            return ret
         else:
-            return None
+            return []
 
 
 def parse_segments(file) -> List[Segment]:
@@ -58,3 +68,13 @@ if __name__ == "__main__":
             if v > 1:
                 count += 1
         print(f"part 1: {count}")
+
+        points = defaultdict(int)
+        for segment in segments:
+            for p in segment.points():
+                points[p] += 1
+        count = 0
+        for k, v in points.items():
+            if v > 1:
+                count += 1
+        print(f"part 2: {count}")
