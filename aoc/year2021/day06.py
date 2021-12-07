000000023474415b391b0158b0d16a67275d09c7
Ziyun Li <me@ziyunli.dev>
2021-12-07 11:10:35 -0500

    day06b

--- aoc/year2021/day06.py
+++ aoc/year2021/day06.py
@@ -1,3 +1,10 @@
+from collections import defaultdict
+from copy import copy
+
+if False:  # TYPE_CHECKCING
+    from typing import DefaultDict, List
+
+
 def parse_lanternfishes(file):
     ret = []
     for line in file:
@@ -25,9 +32,39 @@ def run(initial, days):
     return curr
 
 
+def gen_states(fishes: List[int]) -> DefaultDict[int, int]:
+    states = defaultdict(int)
+    for fish in fishes:
+        states[fish] += 1
+
+    return states
+
+
+def step_states(states: DefaultDict[int, int]) -> DefaultDict[int, int]:
+    new_states = defaultdict(int)
+    for state, count in states.items():
+        if state == 0:
+            new_states[6] += count  # Parents
+            new_states[8] += count  # Children
+        else:
+            new_states[state - 1] += count
+    return new_states
+
+
+def run_states(initial: DefaultDict[int, int], days: int) -> DefaultDict[int, int]:
+    curr = initial
+    for day in range(days):
+        curr = step_states(curr)
+    return curr
+
+
 if __name__ == "__main__":
     with open("input/2021/day06.txt") as reader:
         initial = parse_lanternfishes(reader)
 
-        lanternfishes = run(initial, 80)
-        print(f"part 1: {lanternfishes} -> {len(lanternfishes)}")
+        lanternfishes = run(copy(initial), 80)
+        print(f"part 1: {len(lanternfishes)}")
+
+        initial_states = gen_states(copy(initial))
+        final_states = run_states(initial_states, 256)
+        print(f"part 2: {sum(final_states.values())}")
