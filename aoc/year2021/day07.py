0000000977324bebb0737ccb8110762f1f77757a
Ziyun Li <me@ziyunli.dev>
2021-12-07 21:57:01 -0500

    day07b

--- aoc/year2021/day07.py
+++ aoc/year2021/day07.py
@@ -10,13 +10,40 @@ def parse_numbers(file):
 
 
 def calculate_fuels(positions):
-    median = median_high(positions)
-
+    final_position = median_high(nums)
     s = 0
     for pos in positions:
-        s += abs(pos - median)
+        s += abs(pos - final_position)
 
     return s
+
+
+# def calculate_crab_fuels(positions):
+#     final_position = int(round(mean(nums)))
+
+#     s = 0
+#     for pos in positions:
+#         diff = abs(pos - final_position)
+#         fuel = int(diff * (diff + 1) / 2)
+#         s += fuel
+#     return s
+
+
+def calculate_crab_fuels(positions):
+    final_position = None
+    total_fuels = None
+    min_position = min(positions)
+    max_position = max(positions)
+    for i in range(min_position, max_position + 1):
+        fuels = 0
+        for pos in positions:
+            diff = abs(i - pos)
+            fuel = int(diff * (diff + 1) / 2)
+            fuels += fuel
+        if not total_fuels or total_fuels > fuels:
+            final_position = pos
+            total_fuels = fuels
+    return final_position, total_fuels
 
 
 if __name__ == "__main__":
@@ -24,3 +51,5 @@ if __name__ == "__main__":
         nums = parse_numbers(reader)
 
         print(f"part 1: {calculate_fuels(nums)}")
+
+        print(f"part 2: {calculate_crab_fuels(nums)}")
